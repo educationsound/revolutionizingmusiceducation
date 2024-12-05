06f224c9310ff625fdde68a1e372bb5814e8f9e8
educationsound <38593976+educationsound@users.noreply.github.com>
2024-12-05 08:50:26 -0800

    Add files via upload
    added essential html, css, and js files

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We'll get back to you shortly.");
+    this.reset();
+});
